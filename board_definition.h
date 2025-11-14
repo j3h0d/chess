7bb34d9e142e336bbcdb64d7295ea097cf4453df
Oliver Haux <oliver.l.haux@gmail.com>
2025-11-14 16:27:00 +0100

    Add chess tiles

--- board_definition.h
+++ board_definition.h
@@ -9,18 +9,20 @@
 
 #define VIDEO_DMA_BASE 0x04000100u
 
-#define VIDEO_DMA_BUFFER 0 //request a buffer swap
-#define VIDEO_DMA_BACKBUFFER 1 //next framebuffer to display
-#define VIDEO_DMA_STATUS 3 //status
+#define VIDEO_DMA_BUFFER 0      // request a buffer swap
+#define VIDEO_DMA_BACKBUFFER 1  // next framebuffer to display
+#define VIDEO_DMA_STATUS 3      // status
 
-#define VIDEO_WIDTH 320
+#define VIDEO_WIDTH  320
 #define VIDEO_HEIGHT 240
-#define VIDEO_PITCH 320 //nums of bytes per row
+#define VIDEO_PITCH  320        // bytes per row
 
-// colors
+// colors (RGB 3-3-2)
 
 #define RGB332(r, g, b)  ( ((r) & 0xE0) | (((g) >> 3) & 0x1C) | ((b) >> 6) )
 
-#define COLOR_LIGHT_TILE   RGB332(220, 220, 200) //light square col to test
+#define COLOR_BACKGROUND  RGB332(100, 100, 100)
+#define COLOR_LIGHT_TILE  RGB332(220, 220, 200)
+#define COLOR_DARK_TILE   RGB332( 80,  80,  80)
 
 #endif
--- main.c
+++ main.c
@@ -1,26 +1,44 @@
 #include <stdint.h>
 #include "board_definition.h"
 
-void handle_interrupt(void) //not used for now
+void handle_interrupt(void)
 {
 }
 
 int main(void) {
-    //pointers
-    volatile uint8_t *framebuffer = (volatile uint8_t *)VIDEO_FRAMEBUFFER_BASE;
-    volatile uint32_t *vga_controll = (volatile uint32_t *)VIDEO_DMA_BASE;
+    volatile uint8_t  *framebuffer  = (volatile uint8_t *)VIDEO_FRAMEBUFFER_BASE;
+    volatile uint32_t *vga_control  = (volatile uint32_t *)VIDEO_DMA_BASE;
 
-    //test color
+    // clear entire screen to background color
     for (int y = 0; y < VIDEO_HEIGHT; y++) {
         for (int x = 0; x < VIDEO_WIDTH; x++) {
-            framebuffer[y * VIDEO_PITCH + x] = COLOR_LIGHT_TILE;
+            framebuffer[y * VIDEO_PITCH + x] = COLOR_BACKGROUND;
         }
     }
 
-    //telling the DMA which buffer to display
-    vga_controll[VIDEO_DMA_BACKBUFFER] = (uint32_t)framebuffer;
+    // board geometry: 8x8 board, use full height (240 -> tile size 30)
+    const int tile_size  = VIDEO_HEIGHT / 8;          // 30
+    const int board_size = tile_size * 8;             // 240
+    const int board_x0   = (VIDEO_WIDTH - board_size) / 2; // 40
+    const int board_y0   = 0;                         // top of screen
 
-    vga_controll[VIDEO_DMA_BUFFER] = 0;
+    // draw chessboard
+    for (int y = board_y0; y < board_y0 + board_size; y++) {
+        int tile_y = (y - board_y0) / tile_size;
+        for (int x = board_x0; x < board_x0 + board_size; x++) {
+            int tile_x = (x - board_x0) / tile_size;
+
+            // light/dark based on tile parity
+            uint8_t color =
+                ((tile_x + tile_y) & 1) ? COLOR_DARK_TILE : COLOR_LIGHT_TILE;
+
+            framebuffer[y * VIDEO_PITCH + x] = color;
+        }
+    }
+
+    // tell the DMA which buffer to display
+    vga_control[VIDEO_DMA_BACKBUFFER] = (uint32_t)framebuffer;
+    vga_control[VIDEO_DMA_BUFFER]     = 0;  // request buffer swap
 
     while (1) { }
 
